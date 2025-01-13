e2b77350d4cb2363460abab5ba562347f376dd66
Jillazquez <jillazquez@gmail.com>
2025-01-13 18:24:02 +0100

    test creado

--- view/view.py
+++ view/view.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, HTTPException
 from typing import List
 from handler.handler import (
-    get_water_type_url,
     fetch_water_pokemons,
     fetch_pokemon_by_id,
-    fetch_pokemon_by_type,
+    fetch_pokemons_by_type,
 )
 from models.response import Pokemon  # Asegúrate de que el modelo Pokemon esté bien definido
 from utils.Logger import Logger  # Ajusté la importación del Logger
@@ -13,24 +12,10 @@ logger = Logger()  # Crea una instancia del logger
 
 router = APIRouter()
 
-@router.get("/water_pokemons", response_model=List[Pokemon])
-async def get_water_pokemons():
-    water_url = get_water_type_url()
-    if not water_url:
-        logger.add_to_log("error", "No se encontró el tipo agua")  # Log de error
-        raise HTTPException(status_code=404, detail="No se encontró el tipo agua")
-
-    water_pokemons = await fetch_water_pokemons(water_url)
-    if not water_pokemons:
-        logger.add_to_log("error", "No se encontraron Pokémon de tipo agua")  # Log de error
-        raise HTTPException(status_code=404, detail="No se encontraron Pokémon de tipo agua")
-    
-    return [Pokemon(name=name) for name in water_pokemons]
-
 @router.get("/pokemon/{id}", response_model=Pokemon)
 async def get_pokemon_by_id(id: int):
     """Obtiene un Pokémon por su número (ID)."""
-    pokemon = await fetch_pokemon_by_id(id)
+    pokemon = fetch_pokemon_by_id(id)
     if not pokemon:
         logger.add_to_log("error", f"Pokémon con ID {id} no encontrado")  # Log de error
         raise HTTPException(status_code=404, detail="Pokémon no encontrado")
@@ -39,10 +24,8 @@ async def get_pokemon_by_id(id: int):
 @router.get("/type/{type_name}", response_model=List[Pokemon])
 async def get_pokemon_by_type(type_name: str):
     """Obtiene Pokémon por su tipo."""
-    pokemons = await fetch_pokemon_by_type(type_name)
+    pokemons = fetch_pokemons_by_type(type_name)
     if not pokemons:
         logger.add_to_log("error", f"No se encontraron Pokémon del tipo {type_name}")  # Log de error
         raise HTTPException(status_code=404, detail=f"No se encontraron Pokémon del tipo {type_name}")
     return [Pokemon(name=name) for name in pokemons]
-
-
