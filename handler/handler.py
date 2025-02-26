28d41a46e8f384d3556566defaa6938de4af46f4
Jillazquez <jillazquez@gmail.com>
2025-02-26 18:43:54 +0100

    fix

--- handler/handler.py
+++ handler/handler.py
@@ -20,7 +20,7 @@ redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_client = redis.Redis(host=redis_host, port=6379, db=0, decode_responses=True)
 
 
-type_to_id = {
+TYPE_TO_ID = {
     "normal": 1, "fighting": 2, "flying": 3, "poison": 4, "ground": 5, "rock": 6,
     "bug": 7, "ghost": 8, "steel": 9, "fire": 10, "water": 11, "grass": 12,
     "electric": 13, "psychic": 14, "ice": 15, "dragon": 16, "dark": 17, "fairy": 18
